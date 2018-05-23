9f864602bbe8ce731c2c430de81877d16d965cb2
Madox Sesen <madox.sesen@iteratec.de>
2018-05-23 22:43:23 +0200

    backing up old laptop

--- homepage/settings.py
+++ homepage/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'examplesecret.reset.this'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
